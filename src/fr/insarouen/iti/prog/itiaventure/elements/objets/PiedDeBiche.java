bf92fc2289e9179b0f0e286fbb740bef58b550aa
Taoba <taoba.ouathrani@insa-rouen.fr>
2024-01-29 18:07:07 +0100

    Travail sur PiedDeBiche

---
+++ src/fr/insarouen/iti/prog/itiaventure/elements/objets/PiedDeBiche.java
@@ -0,0 +1,14 @@
+package fr.insarouen.iti.prog.itiaventure.elements.objets;
+
+public class PiedDeBiche extends Objet {
+
+    public PiedDeBiche() {
+
+    }
+
+    @Override
+    public boolean estDeplacable() {
+        return true;
+    }
+
+}
