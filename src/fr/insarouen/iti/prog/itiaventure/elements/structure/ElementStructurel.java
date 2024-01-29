3aacf886d36fd740adb8459e712ca09252e6dfe0
Taoba <taoba.ouathrani@insa-rouen.fr>
2024-01-29 18:14:58 +0100

    Travail sur element structurel

---
+++ src/fr/insarouen/iti/prog/itiaventure/elements/structure/ElementStructurel.java
@@ -0,0 +1,11 @@
+package fr.insarouen.iti.prog.itiaventure.elements.structure;
+
+import fr.insarouen.iti.prog.itiaventure.Monde;
+import fr.insarouen.iti.prog.itiaventure.elements.Entite;
+
+public class ElementStructurel extends Entite {
+
+    public ElementStructurel(String nom, Monde monde) {
+        super(nom, monde);
+    }
+}
