30b3092900805352fd7ad24eb221b54f6eac1317
Alix ANNERAUD <alix@anneraud.fr>
2024-02-05 17:53:39 +0100

    Ecriture de la documentation d'état

--- src/fr/insarouen/iti/prog/itiaventure/elements/Etat.java
+++ src/fr/insarouen/iti/prog/itiaventure/elements/Etat.java
@@ -1,9 +1,27 @@
 package fr.insarouen.iti.prog.itiaventure.elements;
 
+/**
+ * Énumération des états possibles pour une porte.
+ */
 public enum Etat{
+    /**
+     * La porte est cassée.
+     */
     CASSE,
+    /**
+     * La porte est déverrouillée.
+     */
     DEVEROUILLE,
+    /**
+     * La porte est fermée.
+     */
     FERME,
+    /**
+     * La porte est ouverte.
+     */
     OUVERT,
+    /**
+     * La porte est verrouillée.
+     */
     VERROUILLE
 }
