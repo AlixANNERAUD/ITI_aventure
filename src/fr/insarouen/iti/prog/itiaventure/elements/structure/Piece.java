9c866a6ee1c1b4252b653b77d5fbd3085453f82b
Alix ANNERAUD <alix@anneraud.fr>
2024-02-05 17:50:59 +0100

    Ecriture de la documentation de pièce

--- src/fr/insarouen/iti/prog/itiaventure/elements/structure/Piece.java
+++ src/fr/insarouen/iti/prog/itiaventure/elements/structure/Piece.java
@@ -4,19 +4,44 @@ import fr.insarouen.iti.prog.itiaventure.Monde;
 import fr.insarouen.iti.prog.itiaventure.elements.objets.Objet;
 import fr.insarouen.iti.prog.itiaventure.elements.vivants.Vivant;
 
+/**
+ * Classe représentant une pièce.
+ */
 public class Piece extends ElementStructurel {
 
+    /**
+     * Tableau contenant les objets de la pièce.
+     */
     private Objet[] objets = new Objet[0];
+
+    /**
+     * Tableau contenant les vivants de la pièce.
+     */
     private Vivant[] vivants = new Vivant[0];
 
+    /**
+     * Constructeur Piece.
+     * @param nom Nom de la pièce.
+     * @param monde Monde dans lequel se trouve la pièce.
+     */
     public Piece(String nom, Monde monde) {
         super(nom, monde);
     }
 
+    /**
+     * Cette méthode vérifie si la pièce contient un objet.
+     * @param objet Objet à vérifier.
+     * @return true si la pièce contient l'objet, false sinon.
+     */
     public boolean contientObjet(Objet objet) {
         return this.contientObjet(objet.getNom());
     }
 
+    /**
+     * Cette méthode vérifie si la pièce contient un objet.
+     * @param nomObjet Nom de l'objet à vérifier.
+     * @return true si la pièce contient l'objet, false sinon.
+     */
     public boolean contientObjet(String nomObjet) {
         for (int i = 0; i < this.objets.length; i++) {
             if (this.objets[i].getNom().equals(nomObjet)) {
@@ -27,6 +52,11 @@ public class Piece extends ElementStructurel {
 
     }
 
+    /**
+     * Cette méthode vérifie si la pièce contient un vivant.
+     * @param nomVivant Nom du vivant à vérifier.
+     * @return true si la pièce contient le vivant, false sinon.
+     */
     public boolean contientVivant(String nomVivant) {
         for (int i = 0; i < this.vivants.length; i++) {
             if (this.vivants[i].getNom().equals(nomVivant)) {
@@ -37,10 +67,19 @@ public class Piece extends ElementStructurel {
 
     }
 
+    /**
+     * Cette méthode vérifie si la pièce contient un vivant.
+     * @param vivant Vivant à vérifier.
+     * @return true si la pièce contient le vivant, false sinon.
+     */
     public boolean contientVivant(Vivant vivant) {
         return this.contientVivant(vivant.getNom());
     }
 
+    /**
+     * Cette méthode dépose un objet dans la pièce.
+     * @param objet Objet à déposer.
+     */
     public void deposer(Objet objet) {
         Objet[] objets = new Objet[this.objets.length + 1];
 
@@ -53,6 +92,10 @@ public class Piece extends ElementStructurel {
         this.objets = objets;
     }
 
+    /**
+     * Cette méthode fait entrer un vivant dans la pièce.
+     * @param vivant Vivant à faire entrer.
+     */
     public void entrer(Vivant vivant) {
         Vivant[] vivants = new Vivant[this.vivants.length + 1];
 
@@ -65,10 +108,20 @@ public class Piece extends ElementStructurel {
         this.vivants = vivants;
     }
 
+    /**
+     * Cette méthode retire un objet de la pièce.
+     * @param objet Objet à retirer.
+     * @return L'objet retiré, null si non trouvé.
+     */
     public Objet retirer(Objet objet) {
         return this.retirer(objet.getNom());
     }
 
+    /**
+     * Cette méthode retire un objet de la pièce.
+     * @param nomObjet Nom de l'objet à retirer.
+     * @return L'objet retiré, null si non trouvé.
+     */
     public Objet retirer(String nomObjet) {
         Objet[] objets = new Objet[this.objets.length - 1];
         Objet objet = null;
@@ -90,7 +143,10 @@ public class Piece extends ElementStructurel {
 
     }
 
-    
+    /**
+     * Cette fonction retourne le tableau contenant les objets de la pièce.
+     * @return
+     */
     public Objet[] getObjets() {
         return this.objets.clone();
     }
