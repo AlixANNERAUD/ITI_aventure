6eae30dd3e660d80053a179971a17a039d03bc07
Alix ANNERAUD <alix@anneraud.fr>
2024-02-12 15:56:55 +0100

    Correction d'élément structurel

--- src/fr/insarouen/iti/prog/itiaventure/elements/structure/ElementStructurelException.java
+++ src/fr/insarouen/iti/prog/itiaventure/elements/structure/ElementStructurelException.java
@@ -1,5 +1,7 @@
 package fr.insarouen.iti.prog.itiaventure.elements.structure;
 
+import fr.insarouen.iti.prog.itiaventure.ITIAventureException;
+
 public class ElementStructurelException extends ITIAventureException{
 
     public ElementStructurelException(String message){
