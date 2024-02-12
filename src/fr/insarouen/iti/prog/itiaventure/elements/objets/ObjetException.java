5bedd0b8341099951d0ebf92674e0cb47d370b79
Taoba <taoba.ouathrani@insa-rouen.fr>
2024-02-12 15:51:14 +0100

    exception  objet

---
+++ src/fr/insarouen/iti/prog/itiaventure/elements/objets/ObjetException.java
@@ -0,0 +1,14 @@
+package fr.insarouen.iti.prog.itiaventure.elements.objets;
+
+import fr.insarouen.iti.prog.itiaventure.ITIAventureException;
+
+public class ObjetException extends ITIAventureException {
+
+    public ObjetException(String message) {
+        super(message);
+    }
+
+    public ObjetException() {
+        super();
+    }
+}
---
+++ src/fr/insarouen/iti/prog/itiaventure/elements/objets/ObjetNonDeplacableException.java
@@ -0,0 +1,12 @@
+package fr.insarouen.iti.prog.itiaventure.elements.objets;
+
+public class ObjetNonDeplacableException extends ObjetException{
+
+    public ObjetNonDeplacableException(String message){
+        super(message);
+    }
+
+    public ObjetNonDeplacableException(){
+        super();
+    }
+}
