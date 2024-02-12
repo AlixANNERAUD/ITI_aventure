92d63983a4a7dc1a59b0c5a24854549803de25d7
Taoba <taoba.ouathrani@insa-rouen.fr>
2024-02-12 16:00:39 +0100

    perte fermee exception

---
+++ src/fr/insarouen/iti/prog/itiaventure/elements/structure/PorteFermeException.java
@@ -0,0 +1,13 @@
+package fr.insarouen.iti.prog.itiaventure.elements.structure;
+
+public class PorteFermeException extends ElementStructurelException {
+
+    public PorteFermeException(String message) {
+        super(message);
+    }
+
+    public PorteFermeException() {
+        super();
+    }
+
+}
