9717215e03a01907be4241aae40aa158728ebafc
Taoba <taoba.ouathrani@insa-rouen.fr>
2024-02-12 15:54:12 +0100

    exception elementstructurel

---
+++ src/fr/insarouen/iti/prog/itiaventure/elements/structure/ElementStructurelException.java
@@ -0,0 +1,12 @@
+package fr.insarouen.iti.prog.itiaventure.elements.structure;
+
+public class ElementStructurelException extends ITIAventureException{
+
+    public ElementStructurelException(String message){
+        super(message);
+    }
+    
+    public ElementStructurelException(){
+        super();
+    }
+}
